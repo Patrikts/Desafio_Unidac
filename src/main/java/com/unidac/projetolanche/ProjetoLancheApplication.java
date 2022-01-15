4e8d76d43e7e849a68391ae02b5e986c50eec692
Patrikts <trikteixeira@gmail.com>
2022-01-14 23:34:16 -0300

    Projeto criado

---
+++ src/main/java/com/unidac/projetolanche/ProjetoLancheApplication.java
@@ -0,0 +1,13 @@
+package com.unidac.projetolanche;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoLancheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoLancheApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unidac/projetolanche/ProjetoLancheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unidac.projetolanche;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoLancheApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
